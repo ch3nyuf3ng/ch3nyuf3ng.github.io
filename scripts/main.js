9d1d38ab0406da2fcea18c29f0d34bc8a95bef85
陈宇风 Chen Yufeng <ch3nyuf3ng@icloud.com>
2024-01-17 21:58:37 +0800

    MDN Web Getting Started Practice

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let myImage = document.querySelector("img");
+myImage.onclick = () => {
+    let mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-logo.svg") {
+        myImage.setAttribute("src", "images/firefox-logo2.png")
+    } else {
+        myImage.setAttribute("src", "images/firefox-logo.svg")
+    }
+}
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = "Mozilla 很酷，" + myName;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla 很酷，" + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
